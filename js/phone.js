0c7ac07ef6c0df5791700e8c56f47ca6a6d2b906
shakhawat-ullah <shakhawatullah94@gmail.com>
2024-10-21 20:59:48 +0600

    content-updated

--- js/phone.js
+++ js/phone.js
@@ -1,28 +1,29 @@
-const loadData = async (searchText) => {
+const loadPhone = async (searchText) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
-    // console.log(phones)
+    // console.log(phones);
     displayPhones(phones);
 }
 
-// loadData();
+const displayPhones = phones => {
 
-const displayPhones = (phones) => {
     // console.log(phones);
-
+    // 1. Create Where to add
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.textContent = '';
-    phones.forEach(phone => {
-        // console.log(phone);
 
+    phones.forEach(phone => {
+        console.log(phone)
+
+        //2. What will be added
         const phoneCard = document.createElement('div');
-        phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
+        phoneCard.classList = `card bg-gray-100 p-4 shadow-xl  `;
         phoneCard.innerHTML = `
         <figure>
-            <img src="${phone.image}"/>
+        <img src=${phone.image}
+        alt="Phones" />
         </figure>
-
         <div class="card-body">
             <h2 class="card-title">${phone.phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
@@ -30,17 +31,23 @@ const displayPhones = (phones) => {
                 <button class="btn btn-primary">Buy Now</button>
             </div>
         </div>
-        
         `
+        // 3. append your data
+
         phoneContainer.appendChild(phoneCard);
 
     })
+
+
 }
 
+// loadPhone()
+
+// Handle Search 
 
 const handleSearch = () => {
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    console.log(searchText);
-    loadData(searchText);
+    const inputField = document.getElementById('input-field');
+    const searchText = inputField.value;
+    loadPhone(searchText);
+
 }
